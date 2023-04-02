c4752029b2c9c2cfdc644635b60e23071c706e0e
Sarah Kessi <2156390@cmontmorency.qc.ca>
2023-04-02 17:53:00 -0400

    Fichier app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+const coursRoute = require("./routes/cours-routes");
+const professeursRoute = require("./routes/professeurs-routes");
+const HttpErreur = require("/models/http-erreur");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api/cours", coursRoute);
+app.use("api/professeurs", professeursRoute);
+
+app.use((error, requete, reponse, next) => {
+  return next(new HttpErreur("Route non trouvée", 404));
+});
+
+app.use((error, requete, reponse, next) => {
+  if (reponse.headerSent) {
+    return next(error);
+  }
+  reponse.status(error.code || 500);
+  reponse.json({
+    message: error.message || "Une erreur inconnue est survenue",
+  });
+});
+
+mongoose
+.connect("mongodb://127.0.0.1:27017/")
+.then(() => {
+  app.listen(5000)
+  console.log("Connexion à la base de données réussie");
+})
+.catch(erreur => {
+  console.log(erreur);
+});
+
+app.listen(5000);
