ce73f1b1e265d433b367a02f35339fcc1cac4f6d
Sarah Kessi <2156390@cmontmorency.qc.ca>
2023-04-03 12:58:10 -0400

    Schema de professeur

---
+++ models/professeur.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const professeurSchema = new Schema({
+    nom:{type: String, required: true},
+    prenom:{type: String, required: true},
+    image:{type: String, required: true},
+    dateEmbauche:{type: Date, required: true},
+    cours: [{type: mongoose.Types.ObjectId, required: true, ref:"Cours"}]
+});
+
+module.exports = mongoose.model("Professeur", professeurSchema);
